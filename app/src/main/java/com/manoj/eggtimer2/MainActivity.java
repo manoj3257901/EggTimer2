6a2bf0f065dad3efa4e35dde292095b030e44bdf
ManojKumar <manoj.kumar_bca18@gla.ac.in>
2020-05-15 20:28:16 +0530

    Seekbar and and ImageView applied

---
+++ app/src/main/java/com/manoj/eggtimer2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.manoj.eggtimer2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        TextView timerTextView = findViewById(R.id.timerTextView);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
